64ce1de25d2326f16d802eb8b379555a8692bd7d
MarkVeber <markveber100@gmail.com>
2023-09-09 10:51:38 +0200

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -3,11 +3,13 @@ import random
 
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
+print("Üdvözlöm a cigány jelszó generátorban, itt random jelszavakat generálhat magának, ctrl + c kombinációval léphet ki, viszlát.")
+
 def generate_random_password():
-	length = int(input("lenght: "))
+	length = int(input("Add meg a jelszó hosszát: "))
 
 	random.shuffle(characters)
-	
+
 	password = []
 	for i in range(length):
 		password.append(random.choice(characters))
@@ -15,6 +17,8 @@ def generate_random_password():
 	random.shuffle(password)
 
 	print("".join(password))
+
+	generate_random_password()
 
 
 generate_random_password()
